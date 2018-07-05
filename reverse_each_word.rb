b3c2b4e941a51ac176a5d72ea54ffe918c078fdd
Pan Cheng <pchengeducation@hotmail.com>
2018-07-05 16:10:33 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,8 @@
 
 def reverse_each_word(array)
   new_array = array.split(" ")
-  
-  resersed_array = new_array.each {|x| x.reverse!}
-  
+
+  reverse_array = new_array.each {|x| x.reverse!}
+
   return reverse_array.join(" ")
 end
