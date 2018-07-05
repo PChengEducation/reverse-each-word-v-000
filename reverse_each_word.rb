01749a38f23e3e258c0e20fb79da68b6a8b22d31
Pan Cheng <pchengeducation@hotmail.com>
2018-07-05 16:01:29 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,9 @@
 def reverse_each_word(array = "Hello there, and how are you?")
   array.split
   reverse_array = []
-  return array
+  array.each do |element|
+    reverse_array.push
+    array.shift
+  end
+  return reverse_array
 end
