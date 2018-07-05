e91bd89f4994e460f4e7819a21403700eba34943
Pan Cheng <pchengeducation@hotmail.com>
2018-07-05 16:03:39 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
-def reverse_each_word(array = "Hello there, and how are you?")
-  array.split(",")
+def reverse_each_word(Word = "Hello there, and how are you?")
+  array = word.split(",")
   reverse_array = []
   array.each do |element|
     reverse_array.push
